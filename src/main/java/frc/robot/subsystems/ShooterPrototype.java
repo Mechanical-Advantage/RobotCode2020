37ee705acb8244268df5429e6b528cf3dd7f72f0
e-bonner <e-bonner@users.noreply.github.com>
2020-01-23 19:56:33 -0500

    shooter prototype: fix bugs, add speed

--- src/main/java/frc/robot/subsystems/ShooterPrototype.java
+++ src/main/java/frc/robot/subsystems/ShooterPrototype.java
@@ -7,23 +7,30 @@
 
 package frc.robot.subsystems;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterPrototype extends SubsystemBase {
 
   private static final double defaultRampRate = 10;
-  private static final boolean invertFlywheel = false;
+  private static final boolean invertFlywheel = true;
   private static final boolean invertRollers = false;
 
   CANSparkMax flywheelMaster;
   CANSparkMax flywheelFollower;
   CANSparkMax rollerMaster;
   CANSparkMax rollerFollower;
+  CANEncoder flywheelEncoder;
 
   private Double lastRampRate = null; // Force this to be updated once
 
@@ -35,24 +42,37 @@ public class ShooterPrototype extends SubsystemBase {
     flywheelMaster = new CANSparkMax(3, MotorType.kBrushless);
     flywheelFollower = new CANSparkMax(13, MotorType.kBrushless);
     flywheelFollower.follow(flywheelMaster, true);
-    rollerMaster = new CANSparkMax(5, MotorType.kBrushless);
+    rollerMaster = new CANSparkMax(4, MotorType.kBrushless);
     rollerFollower = new CANSparkMax(11, MotorType.kBrushless);
     rollerFollower.follow(rollerMaster, true);
+    flywheelEncoder = flywheelMaster.getEncoder();
 
     // Stop by default
-    this.setDefaultCommand(new InstantCommand(() -> {
-      this.runFlywheel(0);
-      this.runRollers(0);
-    }, this));
+    final ShooterPrototype subsystem = this;
+    this.setDefaultCommand(new Command() {
+      @Override
+      public Set<Subsystem> getRequirements() {
+        HashSet<Subsystem> set = new HashSet<Subsystem>();
+        set.add(subsystem);
+        return set;
+      }
+
+      @Override
+      public void execute() {
+        subsystem.runFlywheel(0);
+        subsystem.runRollers(0);
+      }
+    });
   }
 
   @Override
   public void periodic() {
     double currentRampRate = SmartDashboard.getNumber("Shooter Prototype/ramp rate", defaultRampRate);
-    if (currentRampRate != lastRampRate) {
+    if (lastRampRate != null && currentRampRate != lastRampRate) {
       flywheelMaster.setOpenLoopRampRate(currentRampRate);
       lastRampRate = currentRampRate;
     }
+    SmartDashboard.putNumber("Shooter Prototype/speed", getSpeed());
   }
 
   public void runFlywheel(double power) {
@@ -61,5 +81,9 @@ public class ShooterPrototype extends SubsystemBase {
 
   public void runRollers(double power) {
     rollerMaster.set(power * (invertRollers ? -1 : 1));
+  }
+
+  public double getSpeed() {
+    return flywheelEncoder.getVelocity() * 1.5;
   }
 }
