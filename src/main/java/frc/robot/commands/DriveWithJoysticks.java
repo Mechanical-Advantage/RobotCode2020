21e883572acf74c3a56ed1e157b74a646c3f6808
e-bonner <e-bonner@users.noreply.github.com>
2020-01-25 21:21:12 -0500

    Remove interrupted in DriveWithJoysticks

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -172,11 +172,6 @@ public class DriveWithJoysticks extends CommandBase {
     return false;
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  protected void interrupted() {
-  }
-
   public static enum JoystickMode {
     Tank, SplitArcade, SplitArcadeRightDrive, Trigger;
   }
