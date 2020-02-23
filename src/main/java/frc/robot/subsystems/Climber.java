d2f641a0c222ad2b15cfb11da4984a2fb044b4aa
abbydillon <dillon.abby@gmail.com>
2020-02-22 19:26:08 -0600

    Started Climber subsystem

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,93 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.RobotType;
+
+public class Climber extends SubsystemBase {
+
+  private static final int currentLimit = 30;
+  private static final int masterDeviceID = 6;
+  private static final int followerDeviceID = 10;
+  private static final int solenoidChannel = 1;
+  private static final int PCM = 0;
+
+  CANSparkMax climberMaster;
+  CANSparkMax climberFollower;
+
+  private Solenoid climberSolenoid;
+
+  /**
+   * Creates a new climber.
+   */
+  public Climber() {
+    if (Constants.getRobot() != RobotType.ROBOT_2020 && Constants.getRobot() != RobotType.ROBOT_2020_DRIVE) {
+      return;
+    }
+
+    climberSolenoid = new Solenoid(solenoidChannel, PCM);
+
+    climberMaster = new CANSparkMax(masterDeviceID, MotorType.kBrushless);
+    climberFollower = new CANSparkMax(followerDeviceID, MotorType.kBrushless);
+
+    climberMaster.restoreFactoryDefaults();
+    climberFollower.restoreFactoryDefaults();
+    climberFollower.follow(climberMaster, true);
+    climberMaster.setSmartCurrentLimit(currentLimit);
+    climberFollower.setSmartCurrentLimit(currentLimit);
+
+    // Stop by default
+    final Climber subsystem = this;
+    this.setDefaultCommand(new Command() {
+      @Override
+      public Set<Subsystem> getRequirements() {
+        HashSet<Subsystem> set = new HashSet<Subsystem>();
+        set.add(subsystem);
+        return set;
+      }
+
+      @Override
+      public void execute() {
+        subsystem.run(0);
+      }
+    });
+    climberMaster.burnFlash();
+    climberFollower.burnFlash();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void run(double power) {
+    if (climberMaster == null) {
+      return;
+    }
+    climberMaster.set(power);
+  }
+
+  public void extend() {
+    if (climberSolenoid == null) {
+      return;
+    }
+    climberSolenoid.set(true);
+  }
+}
