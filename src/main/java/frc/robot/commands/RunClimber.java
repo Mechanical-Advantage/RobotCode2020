39eccfb71015882df76cdabe136fea19505d3066
e-bonner <e-bonner@users.noreply.github.com>
2020-02-29 18:35:12 -0500

    Increase climber deadband

--- src/main/java/frc/robot/commands/RunClimber.java
+++ src/main/java/frc/robot/commands/RunClimber.java
@@ -14,7 +14,7 @@ import frc.robot.subsystems.Climber;
 
 public class RunClimber extends CommandBase {
 
-  private static final double deadband = 0.05;
+  private static final double deadband = 0.07;
 
   private final Climber climber;
   private final DoubleSupplier stickAccess;
