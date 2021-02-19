6af475270f2565496abb742fa62b5bf5109bbad0
Jonah Bonner <jonahwbonner@gmail.com>
2021-02-19 15:36:41 -0500

    Avoid moving hood when multiple positions available

--- src/main/java/frc/robot/commands/RunShooterAtDistance.java
+++ src/main/java/frc/robot/commands/RunShooterAtDistance.java
@@ -19,7 +19,9 @@ import frc.robot.util.PressureSensor;
 
 public class RunShooterAtDistance extends CommandBase {
   private static final double maxWallDistance = 115; // inches to center of robot
+  private static final double minLineDistance = 100; // inches to center of robot
   private static final double maxLineDistance = 220; // inches to center of robot
+  private static final double minTrenchDistance = 180; // inches to center of robot
   private static final double maxFlywheelSpeed = 6500; // RPM
 
   private static final PolynomialRegression wallRegression = new PolynomialRegression(
@@ -90,10 +92,29 @@ public class RunShooterAtDistance extends CommandBase {
     // Determine zone
     HoodPosition bestPosition;
     PolynomialRegression regression;
-    if (distance < maxWallDistance) {
+    double wallLineTransition, lineTrenchTransition;
+    switch (lastCommandedPosition) {
+      case WALL:
+        wallLineTransition = maxWallDistance;
+        lineTrenchTransition = maxLineDistance;
+        break;
+      case LINE:
+        wallLineTransition = minLineDistance;
+        lineTrenchTransition = maxLineDistance;
+        break;
+      case TRENCH:
+        wallLineTransition = minLineDistance;
+        lineTrenchTransition = minTrenchDistance;
+        break;
+      default:
+        wallLineTransition = (maxWallDistance + minLineDistance) / 2;
+        lineTrenchTransition = (maxLineDistance + minTrenchDistance) / 2;
+        break;
+    }
+    if (distance < wallLineTransition) {
       bestPosition = HoodPosition.WALL;
       regression = wallRegression;
-    } else if (distance < maxLineDistance) {
+    } else if (distance < lineTrenchTransition) {
       bestPosition = HoodPosition.LINE;
       regression = lineRegression;
     } else {
