312e3b03a82fd6d4a43e298de5d188ff428a5d57
Jonah Bonner <jonahwbonner@gmail.com>
2021-02-19 21:35:05 -0500

    Finish basic vision command

--- src/main/java/frc/robot/commands/RunGalacticSearchVision.java
+++ src/main/java/frc/robot/commands/RunGalacticSearchVision.java
@@ -7,7 +7,12 @@ package frc.robot.commands;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.RobotOdometry;
+import frc.robot.subsystems.drive.DriveTrainBase;
 import frc.robot.util.GalacticSearchPipeline;
 
 public class RunGalacticSearchVision extends CommandBase {
@@ -15,14 +20,42 @@ public class RunGalacticSearchVision extends CommandBase {
   private final GalacticSearchPipeline pipeline = new GalacticSearchPipeline();
   private Mat image;
 
+  private GalacticSearchPath path;
+  private Command runGalacticSearchABlue;
+  private Command runGalacticSearchARed;
+  private Command runGalacticSearchBBlue;
+  private Command runGalacticSearchBRed;
+
   /** Creates a new RunGalacticSearchVision. */
-  public RunGalacticSearchVision() {
+  public RunGalacticSearchVision(RobotOdometry odometry, DriveTrainBase driveTrain) {
     // Use addRequirements() here to declare subsystem dependencies.
+    runGalacticSearchABlue = new RunGalacticSearchABlue(odometry, driveTrain);
+    runGalacticSearchARed = new RunGalacticSearchARed(odometry, driveTrain);
+    runGalacticSearchBBlue = new RunGalacticSearchBBlue(odometry, driveTrain);
+    runGalacticSearchBRed = new RunGalacticSearchBRed(odometry, driveTrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    if (path == null) {
+      DriverStation.reportWarning("There is no path selected", false);
+    } else {
+      switch (path) {
+        case A_BLUE:
+          runGalacticSearchABlue.schedule();
+          break;
+        case A_RED:
+          runGalacticSearchARed.schedule();
+          break;
+        case B_BLUE:
+          runGalacticSearchBBlue.schedule();
+          break;
+        case B_RED:
+          runGalacticSearchBRed.schedule();
+          break;
+      }
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -33,6 +66,10 @@ public class RunGalacticSearchVision extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    runGalacticSearchABlue.cancel();
+    runGalacticSearchARed.cancel();
+    runGalacticSearchBBlue.cancel();
+    runGalacticSearchBRed.cancel();
   }
 
   // Returns true when the command should end.
@@ -47,7 +84,51 @@ public class RunGalacticSearchVision extends CommandBase {
 
       // Run the GRIP pipeline
       pipeline.process(image);
-      Mat output = pipeline.hsvThresholdOutput();
+      Mat hsvThreshold = pipeline.hsvThresholdOutput();
+
+      // Check for balls
+      if (checkForBall(hsvThreshold, 0.5, 0, 0, 3, 3)) {
+        path = GalacticSearchPath.A_BLUE;
+        SmartDashboard.putString("Path", "A BLUE");
+      } else if (checkForBall(hsvThreshold, 0.5, 3, 3, 6, 6)) {
+        path = GalacticSearchPath.A_RED;
+        SmartDashboard.putString("Path", "A RED");
+      } else if (checkForBall(hsvThreshold, 0.5, 6, 6, 9, 9)) {
+        path = GalacticSearchPath.B_BLUE;
+        SmartDashboard.putString("Path", "B BLUE");
+      } else {
+        path = GalacticSearchPath.B_RED;
+        SmartDashboard.putString("Path", "B RED");
+      }
     }
+  }
+
+  /**
+   * Checks if a ball is in the specified area based on whether the # of white
+   * pixels in an area is greater than a threshold. Origin in the upper left
+   * 
+   * @param source         The source image
+   * @param whiteThreshold The % of the area that must be white
+   * @param x1             Left column
+   * @param y1             Top row
+   * @param x2             Right column
+   * @param y2             Bottom row
+   */
+  private boolean checkForBall(Mat source, double whiteThreshold, int x1, int y1, int x2, int y2) {
+    Mat submat = source.submat(y1, y2, x1, x2);
+    int whiteCount = 0;
+    for (int x = 0; x < submat.width(); x++) {
+      for (int y = 0; y < submat.height(); y++) {
+        if (source.get(y, x)[0] > 0) {
+          whiteCount++;
+
+        }
+      }
+    }
+    return whiteCount / (submat.width() * submat.height()) > whiteThreshold;
+  }
+
+  public static enum GalacticSearchPath {
+    A_BLUE, A_RED, B_BLUE, B_RED
   }
 }
